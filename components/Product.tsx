783e5a4be6b49ff83d5259981ea18ad07dad86bb
Sougata <sougatadas9874@gmail.com>
2024-09-13 20:40:45 +0530

    add products section.

---
+++ components/Product.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import Tube from "@/assets/tube.png";
+import Pyramid from "@/assets/pyramid.png";
+import ProductImage from "@/assets/product-image.png";
+
+const Product = () => {
+  return (
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="section-container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Boost your productivity
+            </div>
+          </div>
+          <h2 className="text-center text-3xl md:text-5xl md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive,
+            SaaS website in just minutes with this template.
+          </p>
+        </div>
+
+        {/* Images */}
+        <div className="relative">
+          <Image
+            priority
+            unoptimized
+            quality={100}
+            src={ProductImage}
+            alt="Product image"
+            height={220}
+            width={220}
+            className="mt-10 max-w-full w-full"
+          />
+
+          <Image
+            priority
+            unoptimized
+            quality={100}
+            src={Pyramid}
+            alt="Pyramid image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+
+          <Image
+            priority
+            unoptimized
+            quality={100}
+            src={Tube}
+            alt="Tube image"
+            height={248}
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Product;
