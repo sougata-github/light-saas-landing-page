ffd5d4ddd71a9ff7249b59226a5f10a6bd0d154b
Sougata <sougatadas9874@gmail.com>
2024-09-14 21:40:25 +0530

    add animation to CTA.

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import Button from "./Button";
 
-import Image from "next/image";
 import Star from "@/assets/star.png";
 import Spring from "@/assets/spring.png";
+import { useRef } from "react";
 
 const CTA = () => {
+  const ref = useRef<HTMLElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [-80, 80]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+      ref={ref}
+    >
       <div className="section-container">
         <div className="max-w-[540px] mx-auto relative">
           <h2 className="section-heading">Sign Up for free today</h2>
@@ -16,25 +32,25 @@ const CTA = () => {
           </p>
 
           {/* Images */}
-          <Image
-            priority
-            unoptimized
-            quality={100}
+          <motion.img
             height={360}
             width={360}
-            src={Star}
+            src={Star.src}
             alt="Star image"
             className="hidden md:block absolute -left-[350px] -top-[137px]"
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            priority
-            unoptimized
-            quality={100}
+          <motion.img
             height={360}
             width={360}
-            src={Spring}
+            src={Spring.src}
             alt="Star image"
             className="hidden md:block absolute -right-[331px] -top-[19px]"
+            style={{
+              translateY,
+            }}
           />
         </div>
 
